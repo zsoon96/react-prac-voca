268f4a9ea202d59b23b42a5380524b455b7949d4
zsoon96 <pres0616@gmail.com>
2022-09-05 21:39:55 +0900

    스타일 적용 방법

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import './App.css';
-import {createRef, useRef, useState} from "react";
+import styled from 'styled-components';
 
 function App() {
 
-    // useRef는 dom을 변경할 때 사용
-    const myRef = useRef(null)
+    const style = {
+        backgroundColor: 'red'
+    }
 
-    const [list, setList] =useState([
-        { id: 1, name: "꺽정"},
-        { id: 2, name: "길동"}
-    ])
-
-    // Array.from()을 사용하여 list의 길이만큼 배열 생성 후, createRef()로 list의 Ref 동적 생성
-    const myRefs = Array.from({length: list.length}).map( ()=> createRef() )
+    const Title = styled.h1`
+        font-size: 1.5em;
+        text-align: center;
+        color: violet;
+    `;
 
     // 렌더링 시점은 상태값이 변할 때
     return (
         <div>
-            <button onClick={ ()=>{
-                // myRef.current.style.backgroundColor = 'blue';
-                myRefs[0].current.style.backgroundColor = 'blue'
-            } }>색 변경</button>
-            <div ref={myRef}>박스</div>
+            {/* 스타일 적용 방법 1 - 비추 */}
+            <div style={style}>파일 '내부'에서 작성</div>법
 
-            {list.map( (u, idx) => <h1 ref={myRefs[idx]}>{u.name}</h1> )}
+            {/* 스타일 적용 방법 2 */}
+            <div className={"box_style"}>파일 '외부'에서 작성</div>
+
+            {/* 스타일 적용 방법 3 */}
+            <Title> 라이브러리를 통해 작성 </Title>
         </div>
     );
 }
