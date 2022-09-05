74c0b51c41861879c058fe7fbc19d69f496de05f
zsoon96 <pres0616@gmail.com>
2022-09-06 06:47:14 +0900

    styled component props passing 수정

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -14,14 +14,14 @@ const Home = (props) => {
 
     // 부모로 부터 받아온 데이터의 스타일링을 동적으로 !
     const StyledDeleteButton = styled.button`
-        color: ${ () => (user.name === 'soon' ? 'blue' : 'red')}
+        color: ${ (props) => (props.user.name === 'soon' ? 'blue' : 'red')}
     `
 
     return (
         <div>
             <h1>홈 : {number}</h1>
             <button onClick={ () => setNumber(number+1) }>숫자 증가</button>
-            <StyledDeleteButton onClick={() => setBoard([])}>삭제하기</StyledDeleteButton>
+            <StyledDeleteButton user={user} onClick={() => setBoard([])}>삭제하기</StyledDeleteButton>
             {boards.map((board) =>
                 <h3>
                     제목 : {board.title}, 내용 : {board.content}
