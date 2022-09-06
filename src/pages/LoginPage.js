c6b317eb0d3a667b3167d55060216b381e81e0ef
zsoon96 <pres0616@gmail.com>
2022-09-07 06:31:16 +0900

    useNavigate를 활용한 페이지 이동(v6)

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,5 +1,5 @@
 import Login from "../components/login/Login";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 
 const LoginPage = () => {
     // Header에서 로그인 버튼 눌렀을 때, 연결되는 path의 값(id) 불러오기 (데이터 넘기기)
@@ -8,8 +8,24 @@ const LoginPage = () => {
     const params = useParams();
     console.log(params.id);
 
+    // v5
+    // const history = useHistory();
+
+    // v5
+    const navigate = useNavigate();
+
+    const goBackHandler = () => {
+        navigate(-1)
+    }
+
+    const goHomeHandler = () => {
+        navigate('/')
+    }
+
     return (
         <div>
+            <button onClick={goBackHandler}>뒤로가기</button>
+            <button onClick={goHomeHandler}>홈으로 가기</button>
             <Login />
         </div>
     )
