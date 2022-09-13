ce3a73bc9c2f33dacc19d2173ffa4d156ec2c4c0
zsoon96 <pres0616@gmail.com>
2022-09-13 22:26:26 +0900

    글목록 조회

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -2,12 +2,16 @@ import React from 'react';
 import {Card} from "react-bootstrap";
 import {Link} from "react-router-dom";
 
-const BookItem = () => {
+const BookItem = (props) => {
+
+    // Home.js에서 전달받은 props를 구조분할 방식으로 데이터 할당
+    const {id, title} = props.book
+
     return (
        <Card>
            <Card.Body>
-               <Card.Title> 제목 1</Card.Title>
-               <Link to={"/post/1"} className="btn btn-primary"> 상세보기 </Link>
+               <Card.Title> {title} </Card.Title>
+               <Link to={"/post/" + id} className="btn btn-primary"> 상세보기 </Link>
            </Card.Body>
        </Card>
     );
--- src/pages/book/Home.js
+++ src/pages/book/Home.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import BookItem from "../../components/BookItem";
+import axios from "axios";
 
 const Home = () => {
+
+    const [books, setBooks] = useState([])
+
+    useEffect( ()=> {
+        axios.get('http://localhost:3000/board')
+            .then((res) => setBooks(res.data) )
+    },[] )
+
     return (
         <div>
             <h1> 책 리스트 조회 </h1>
-            <BookItem/>
+            {/* books 배열에 담긴 횟수만큼 BookItem 컴포넌트 실행 */}
+            {/* 데이터의 key와 book이라는 props를 BookItem 컴포넌트로 넘겨줌 */}
+            {books.map( (book) => (<BookItem key={book.id} book={book} />) )}
         </div>
     );
 };
