bc7ad56e22b76ca41ceb1114571710597e27a196
zsoon96 <pres0616@gmail.com>
2022-09-19 06:35:44 +0900

    로그아웃 기능 연결 - 수정

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,9 @@ const Header = () => {
     const logoutHandler = () => {
         axios.post('http://localhost:3000/auth/logout')
             .then((res) => {
-                alert('로그아웃 성공')
+                const { accessToken } = res.data
+                // API 요청할 때마다 헤더에 accessToken 담아 보내도록 설정 -> localStorage, cookie 등에 저장하지 않결
+                axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
                 navigate('/login')
             })
     }
--- src/pages/user/LoginForm.js
+++ src/pages/user/LoginForm.js
@@ -27,7 +27,7 @@ const LoginForm = () => {
         axios.post('http://localhost:3000/auth/login', body)
             .then((res) => {
                 const { accessToken } = res.data
-                // API 요청할 때마다 헤더에 accessToken 담아 보내도록 설정 -> localStorage, cookie 등에 저장하지 않기
+                // API 요청할 때마다 헤더에 accessToken 담아 보내도록 설정 -> localStorage, cookie 등에 저장하지 않결
                 axios.defaults.headers.common['Authorization'] = `Bearer ${accessToken}`;
                 navigate('/')
             })
