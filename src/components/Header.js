5f1c39d4b23dfce97de7f810cb1e1ac377feee4d
zsoon96 <pres0616@gmail.com>
2022-09-19 06:33:14 +0900

    로그아웃 기능 연결

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,20 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
+import axios from "axios";
 
 const Header = () => {
+    const navigate = useNavigate()
+
+    const logoutHandler = () => {
+        axios.post('http://localhost:3000/auth/logout')
+            .then((res) => {
+                alert('로그아웃 성공')
+                navigate('/login')
+            })
+    }
+
     return (
         <>
             <Navbar bg="dark" variant="dark">
@@ -14,6 +25,7 @@ const Header = () => {
                         <Link to="/save" className = "navbar-link">글작성</Link>
                         <Link to="/join" className = "navbar-link">회원가입</Link>
                         <Link to="/login" className = "navbar-link">로그인</Link>
+                        <button onClick={logoutHandler}>로그아웃</button>
                     </Nav>
                 </Container>
             </Navbar>
