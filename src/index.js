855d08f0ff012747f3e6ea78bcdd4da555630899
zsoon96 <pres0616@gmail.com>
2022-09-20 22:36:42 +0900

    redux 활용을 위한 store 등록

--- src/index.js
+++ src/index.js
@@ -3,13 +3,26 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import {BrowserRouter} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux'
+import {applyMiddleware, createStore} from "redux";
+import promiseMiddleware from 'redux-promise'
+import ReduxThunk from 'redux-thunk'
+import Reducer from './reducers'
+
+// 스토어는 기본적으로 객체(object) 타입만 받을 수 있기때문에 redux-thunk 미들웨어를 통해 function, redux-promise를 통해 promise 타입까지 받을 수 있도록 설정
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
-            <App/>
-        </BrowserRouter>
+        {/* 리덕스 적용 = 스토어 등록 */}
+        <Provider store={createStoreWithMiddleware(Reducer,
+            window.__REDUX_DEVTOOLS_EXTENSION__&& widow.__REDUX_DEVTOOLS_EXTENSION__()
+            )}>
+            <BrowserRouter>
+                <App/>
+            </BrowserRouter>
+        </Provider>
     </React.StrictMode>
 );
---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+
+// 각 기능에 따른 리듀서들을 하나로 묶는 역록
+const rootReducer = combineReducers({
+    // user,
+})
+
+export default rootReducer
