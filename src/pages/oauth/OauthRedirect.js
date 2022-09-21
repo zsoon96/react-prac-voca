b8c236e09e7ca1f188ef5dbdc2dff955d0565f7c
zsoon96 <pres0616@gmail.com>
2022-09-22 06:59:34 +0900

    카카오 로그인 연동 - 받은 인가 코드 서버에 전달

--- src/pages/oauth/OauthRedirect.js
+++ src/pages/oauth/OauthRedirect.js
@@ -1,11 +1,13 @@
 import React, {useEffect} from 'react'
+import axios from "axios";
 
 // 리다이렉트 될 화면
 const OauthRedirect = () => {
     const code = new URL (window.location.href).searchParams.get('code')
 
     useEffect(() => {
-        console.log(code)
+        axios.get(`http://localhost:3000/auth/kakao?code=${code}`)
+            .then((res) => console.log(res))
     },[])
 
     return (
