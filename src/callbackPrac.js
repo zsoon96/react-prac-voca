29d84ca84b4e1ebc877ced2335da81cab479e7ed
zsoon96 <pres0616@gmail.com>
2022-10-07 21:44:59 +0900

    JS Callback 함수(비동기)

--- src/callbackPrac.js
+++ src/callbackPrac.js
@@ -26,6 +26,45 @@ findUserAndCallback(1, function (user) {
     // user: {id: 1, name: "User1", email: "1@test.com"}
 })
 
+function findUserCallbackTimeout(id) {
+    let user;
+    // 자바스크립트에서 대표적인 내장 비동기 함수
+    // setTimeout(실행 작업 내용의 콜백 함수, 기다리는 밀리초 단위)
+    setTimeout(function () {
+        console.log("waited 0.1 sec.");
+        user = {
+            id: id,
+            name: 'User' + id,
+            email: id + '@test.com',
+        }
+    }, 100);
+    return user;
+}
+
+const user2 = findUserCallbackTimeout(1);
+console.log("user: ", user2);
+// user: undefined > 비동기 함수의 호출이라 실행이 완료될 때까지 기다리지 않고 바로 return user 값 출력하기 때문
+// waited 0.1 sec.
+
+function findUserAndCallbackTimeout(id, cb) {
+    setTimeout (function () {
+        console.log("waited 0.1 sec.");
+        const user = {
+            id: id,
+            name: 'User' + id,
+            email: id + '@test.com',
+        };
+        cb(user);
+    }, 100);
+    return user;
+}
+
+findUserAndCallbackTimeout(1, function (user) {
+    console.log("user: ", user);
+    // user: {id: 1, name: "User1", email: "1@test.com"}
+    // waited 0.1 sec.
+})
+
 
 const user = findUser(1);
 console.log(user);
